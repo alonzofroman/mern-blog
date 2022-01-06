dae17fd1100ad3c594f0e35afb0b1e51135a6105
Roman <alonzofroman@gmail.com>
2022-01-05 19:41:46 -0800

    added connection to server file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { Db } = require('mongodb');
+const db = require('./config/connection');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 3001
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,3 +11,4 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
