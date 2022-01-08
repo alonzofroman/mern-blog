4aeca052397b9c6d789288367695b98bac6e6de4
Roman <alonzofroman@gmail.com>
2022-01-07 16:17:58 -0800

    Added navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -3,7 +3,10 @@ import React from 'react';
 function Navbar () {
     return (
         <div className='navHead'>
-            
+            <h1>BlogBook</h1>
+            <a href="/user/:id">My Account</a>
         </div>
     )
-}
+};
+
+export default Navbar;
