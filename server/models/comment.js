f7952e4ae0d499ff3ff5719240f15597db0b794e
Roman <alonzofroman@gmail.com>
2022-01-11 15:32:34 -0800

    Added index for models

--- server/models/comment.js
+++ server/models/comment.js
@@ -4,6 +4,14 @@ const commentSchema = new Schema({
     content: {
         type: String,
         required: true,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    post: {
+        type: Schema.Types.ObjectId,
+        ref: 'Post'
     }
 });
 
---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+import User from "./user";
+import Post from "./post";
+import Comment from "./comment";
+
+module.exports = { User, Post, Comment };
