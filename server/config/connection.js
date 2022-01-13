68d5178206207c59df843e3ada881a4f4dc9af2f
Roman <alonzofroman@gmail.com>
2022-01-13 15:34:46 -0800

    Removed deprecated options from connection js

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernblog', {
     useNewUrlParser: true,
-    useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
