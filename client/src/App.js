37d4b958fd671a820808c548daf660773ac88400
Roman <alonzofroman@gmail.com>
2022-01-13 15:39:59 -0800

    Added footer to page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer () {
+    return (
+        <div className='footer'>
+            <p>Made by Alonzo Roman</p>
+        </div>
+    )
+};
+
+export default Footer;
