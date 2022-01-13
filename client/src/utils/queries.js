2d0394f6f3342ba428aee33b861f5aa51638d0e7
Roman <alonzofroman@gmail.com>
2022-01-13 14:03:27 -0800

    Added user queries to client side

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,87 @@
 import { gql } from '@apollo/client';
+
+export const GET_USERS = gql`
+    query users{
+        _id
+        username
+        email
+        password
+        posts {
+            _id
+            title
+            content
+            user {
+                _id
+                username
+            }
+            comments {
+                _id
+                user
+                content
+            }
+        }
+        comments {
+            _id
+            content
+        }
+    }
+`
+
+export const GET_USER_BY_ID = gql`
+    query user($userId: ID!){
+        user(userId: $userId) {
+            _id
+            username
+            email
+            password
+            posts {
+                _id
+                title
+                content
+                user {
+                    username
+                }
+            comments {
+                _id
+                content
+                user {
+                    _id
+                    username
+                }
+            }
+            }
+        }
+    }
+`
+
+export const GET_POSTS = gql`
+    query posts{
+        _id
+        title
+        content
+        user {
+            _id
+            username
+        }
+        comments {
+            _id
+            content
+            user {
+                _id
+                username
+            }
+            post {
+                _id
+            }
+
+        }
+    }
+`
+
+export const GET_POST_BY_ID = gql`
+    query post($postId: ID!){
+        post(postId: $postId) {
+            
+        }
+    }
+`
