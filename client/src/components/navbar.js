cb27e75b7c07b3515141820102e4194a845692cf
Roman <alonzofroman@gmail.com>
2022-01-13 16:33:34 -0800

    Added login and signup links

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -5,6 +5,8 @@ function Navbar () {
         <div className='navHead'>
             <h1>BlogBook</h1>
             <a href="/user/:id">My Account</a>
+            <a href='/login'>Log In</a>
+            <a href='/signup'>Sign Up</a>
         </div>
     )
 };
