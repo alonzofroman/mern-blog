f8202a06f831ba20a6140eb9fe74b486a300dc66
Roman <alonzofroman@gmail.com>
2022-02-08 14:04:49 -0800

    Added connect mongodb session

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,16 @@ import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Login from './components/login';
 import SignUp from './components/signup';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-        
+        <Router>
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={SignUp} />
+        </Router>
       <Footer />
     </div>
   );
