5cf407ea71d9c302aa4fbb817297626cf3e57ef3
Roman <alonzofroman@gmail.com>
2022-02-08 13:51:12 -0800

    Moved login form to its own component page

--- client/src/App.js
+++ client/src/App.js
@@ -3,44 +3,13 @@ import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import Login from './components/login';
+import SignUp from './components/signup';
 
 function App() {
   return (
     <div className='App'>
-      <form>
-        <h3>Sign In</h3>
-        <div className='form-group'>
-          <label>Email address</label>
-          <input
-            type='email'
-            className='form-control'
-            placeholder='Enter email'
-          />
-        </div>
-        <div className='form-group'>
-          <label>Password</label>
-          <input
-            type='password'
-            className='form-control'
-            placeholder='Enter password'
-          />
-        </div>
-        <div className='form-group'>
-          <div className='custom-control custom-checkbox'>
-            <input
-              type='checkbox'
-              className='custom-control-input'
-              id='customCheck1'
-            />
-            <label className='custom-control-label' htmlFor='customCheck1'>
-              Remember me
-            </label>
-          </div>
-        </div>
-        <button type='submit' className='btn btn-primary btn-block'>
-          Submit
-        </button>
-      </form>
+      
     </div>
   );
 }
